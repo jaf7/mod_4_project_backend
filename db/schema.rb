ac07ee530cc760af6f7e5ae2864ef29b65c93a8b
cs-j <cj@mail.co.de>
2018-04-17 15:56:03 -0400

    dropped and re-migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_15_121153) do
+ActiveRecord::Schema.define(version: 2018_04_13_192344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,20 +18,8 @@ ActiveRecord::Schema.define(version: 2018_04_15_121153) do
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.string "title"
     t.text "body"
-    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "project_id"
-    t.string "name"
-    t.index ["project_id"], name: "index_users_on_project_id"
-  end
-
-  add_foreign_key "projects", "users"
-  add_foreign_key "users", "projects"
 end
