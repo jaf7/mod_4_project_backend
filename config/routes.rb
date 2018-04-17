048ba8381da045d967950d818ddf1a1aa6278693
cs-j <cj@mail.co.de>
2018-04-17 13:50:46 -0400

    added root route in config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
   resources :projects, only: [:index, :create, :update]
+  root 'projects#index'
   mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
